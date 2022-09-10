422404b4dac5e1a9224466a7c5cb98ee7fca50e9
Sakuras <113246526+Sakuras114514@users.noreply.github.com>
2022-09-10 22:16:45 +0800

    23
    1

---
+++ Lua/图种.lua
@@ -0,0 +1,9 @@
+headPathname = '/sdcard/1.png' -- 图片路径
+zipPathname = '/sdcard/1.zip' -- 压缩包路径
+newFilePathname = '/sdcard/2.png' -- 新文件路径
+
+zipTwo = io.open(zipPathname, 'rb'):read('*a')
+headTwo = io.open(headPathname, 'rb'):read('*a')
+io.open(newFilePathname, 'wb')
+io.open(newFilePathname, 'ab'):write(headTwo)
+io.open(newFilePathname, 'ab'):write(zipTwo)/
